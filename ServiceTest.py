a18ca486d3b25b367e2c86744bead8e0dd63d7ba
Bruno R. Toffoli <btoffoli@gmail.com>
2015-12-13 01:08:03 -0200

    starting test class

--- ServiceTest.py
+++ ServiceTest.py
@@ -1,8 +1,15 @@
+# -*- coding: utf-8 -*-
 __author__ = 'btoffoli'
 
 from service import *
 
-from unittest import *
+from unittest import TestCase, main as mainTest
+
+"""
+Format Packet waited
+CATRACA|SENSORID
+LEITURA|SENSORID|LIGHTSENSOR
+"""
 
 class ServiceTest(TestCase):
 
@@ -13,9 +20,21 @@ class ServiceTest(TestCase):
         self.service.processarPacote('CATRACA|2')
         self.assertTrue(len(self.service.sensores) > 0, 'Deveria já haver sensor')
 
-        self.as
+        sensor = self.service.sensores[0]
+        self.assertEqual(sensor.id, int(2), 'Id do sensor deveria ser 2')
+
+        self.service.processarPacote('LEITURA|2|300')
+
+        firstData = sensor.lastLeitura()
+        self.assertIsNotNone(firstData, 'Deveria haver leitura')
+
+        self.assertEqual(firstData['valor'], 300)
 
 
 
 
 
+
+
+if __name__ == '__main__':
+    mainTest()
